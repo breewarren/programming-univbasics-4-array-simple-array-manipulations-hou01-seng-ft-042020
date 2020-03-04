630dabaccc8b86e15bf071cdcba682ae428f3023
Bree Warren <ambreea.warren@gmail.com>
2020-03-04 00:16:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -52,9 +52,12 @@ def using_delete(instructors, no_offense_steven)
   instructors.delete("Steven")
 end
 
-def using_delete_at(array, integer)
-  
-  .delete(indexequaltointeger)
+def using_delete_at(famous_robots, 2)
+  famous_robots = ["Johnny 5", "R2D2", "Robocop"]
+  famouns_robots.delete(2)
 end
 
 deleted_robot = using_delete_at(famous_robots, 2)
+
+famous_robots = ["Johnny 5", "R2D2", "Robocop"]
+    expect(deleted_robot).to eq("Robocop")
