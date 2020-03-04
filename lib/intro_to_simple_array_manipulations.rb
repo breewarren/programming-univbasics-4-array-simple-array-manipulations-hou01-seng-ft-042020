f801e06cd31721b6f3519df2cb4a85151c955611
Bree Warren <ambreea.warren@gmail.com>
2020-03-04 00:18:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -52,7 +52,8 @@ def using_delete(instructors, no_offense_steven)
   instructors.delete("Steven")
 end
 
-def using_delete_at(famous_robots, [2])
+def using_delete_at(famous_robots, deleted_robot)
   famous_robots = ["Johnny 5", "R2D2", "Robocop"]
-  famous_robots.delete(2)
+  deleted_robot = [2]
+  famous_robots.delete(deleted_robot)
 end
